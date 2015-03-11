5488a8cb7ad5defb8d2028f2c18da3a615ea910a
Mathias Brandt <maini123@gmail.com>
2015-03-11 16:52:11 +0100

    Added comments

--- connect-four/src/MiniMaxer.java
+++ connect-four/src/MiniMaxer.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+
 /**
  * Created by brandt on 10/03/15.
  */
@@ -13,80 +15,120 @@ public class MiniMaxer {
         aiPlayerId = playerId;
     }
 
+    /**
+     * In the current state, calculate the best possible action.
+     * @param state the current state
+     * @return the best possible action
+     */
     public int minimaxDecision(GameState state) {
-        System.out.println("Making decision for player " + aiPlayerId);
+        // the minimaxValues array will hold the minimax values for each action (i.e., column) in the current state
+        double[] minimaxValues = new double[state.getColumnCount()];
 
+        // the best action to pick
+        int bestAction = 0;
+
+        // initialize alpha and beta with dummy values
         double alpha = Double.NEGATIVE_INFINITY;
         double beta = Double.POSITIVE_INFINITY;
 
-        double[] values = new double[state.getColumnCount()];
-        int action = -1;
-
+        // if the AI is playing as MIN we want to find the lowest minimax value.
         if(aiPlayerId == PLAYER_MIN) {
+            // go through each possible action (i.e., column) in the current state
             for(int i = 0; i < state.getColumnCount(); i++) {
+                // if the action is actually in the list of possible actions, i.e., if the column is NOT full
                 if(!state.isColumnFull(i)) {
-                    values[i] = maxValue(state.copyState(), alpha, beta);
+                    // calculate the minimax value for this action
+                    minimaxValues[i] = maxValue(state.copyState(), alpha, beta);
                 } else {
-                    values[i] = Double.NaN;
+                    // if the action is not in the list of possible actions, assign a value to indicate such.
+                    minimaxValues[i] = Double.NaN;
                 }
             }
 
+            // initialize with dummy value
             double minValue = Double.MAX_VALUE;
-            for(int i = 0; i < values.length; i++) {
-                System.out.println(values[i]);
 
-                if(values[i] < minValue) {
-                    action = i;
-                    minValue = values[i];
+            // go through all of the calculated minimax values to pick the best corresponding action
+            for(int i = 0; i < minimaxValues.length; i++) {
+                //System.out.println(minimaxValues[i]);
+                if(minimaxValues[i] < minValue) {
+                    bestAction = i;
+                    minValue = minimaxValues[i];
                 }
             }
-        } else {
+        }
+        // if the AI is playing as MAX the operations are the same as if the AI was
+        // playing as MIN, except now we want the highest minimax value.
+        // Comments have been omitted.
+        else {
             for(int i = 0; i < state.getColumnCount(); i++) {
                 if(!state.isColumnFull(i)) {
-                    values[i] = minValue(state.copyState(), alpha, beta);
+                    minimaxValues[i] = minValue(state.copyState(), alpha, beta);
                 } else {
-                    values[i] = Double.NaN;
+                    minimaxValues[i] = Double.NaN;
                 }
             }
 
             double maxValue = Double.MIN_VALUE;
-            for(int i = 0; i < values.length; i++) {
-                System.out.println(values[i]);
+            for(int i = 0; i < minimaxValues.length; i++) {
+                System.out.println(minimaxValues[i]);
 
-                if(values[i] > maxValue) {
-                    action = i;
-                    maxValue = values[i];
+                if(minimaxValues[i] > maxValue) {
+                    bestAction = i;
+                    maxValue = minimaxValues[i];
                 }
             }
         }
-        return action;
+
+        return bestAction;
     }
 
+    /**
+     * Returns the lowest utility value for the current state.
+     * @param state the current state
+     * @param alpha the parent node's alpha value
+     * @param beta the parent node's beta value
+     * @return the lowest utility value
+     */
     private double minValue(GameState state, double alpha, double beta) {
         double utility = getUtility(state);
+
+        // if the utility value is bigger than zero, it means that the game is over in the current state.
         if(utility > 0) {
             return utility;
         }
 
+        // initialize with dummy value
         double value = Double.MAX_VALUE;
+
+        // try to perform each possible action and keep track of the best one
         for (int i = 0; i < state.getColumnCount(); i++) {
             if (!state.isColumnFull(i)) {
                 state.insertCoin(i, PLAYER_MIN);
                 value = Double.min(value, maxValue(state.copyState(), alpha, beta));
 
-                beta = Double.min(beta, value);
-
+                // if the value is lower than the parent's alpha value, prune the tree
                 if(value <= alpha){
-                    System.out.println("Pruning");
                     return value;
                 }
+
+                // if not, record new beta value
+                beta = Double.min(beta, value);
             }
         }
 
         return value;
     }
 
+    /**
+     * Returns the highest utility value for the current state.
+     * @param state the current state
+     * @param alpha the parent node's alpha value
+     * @param beta the parent node's beta value
+     * @return the highest utility value
+     */
     private double maxValue(GameState state, double alpha, double beta) {
+        // NOTE: comments have been omitted since they are more or less the same as in the minValue method
         double utility = getUtility(state);
         if(utility > 0) {
             return utility;
@@ -102,7 +144,6 @@ public class MiniMaxer {
                 alpha = Double.max(alpha, value); // TODO: overall alpha or the one supplied?
 
                 if(value >= beta){
-                    System.out.println("Pruning");
                     return value;
                 }
 
@@ -112,6 +153,12 @@ public class MiniMaxer {
         return value;
     }
 
+    /**
+     * If the game has finished in the current state, i.e., the board is full or one of the
+     * players has won, return the corresponding utility value. Otherwise return -1
+     * @param state the current state
+     * @return the utility value of the winning player, or -1 if the game is not over
+     */
     private double getUtility(GameState state) {
         switch(state.gameFinished()) {
             case PLAYER1:
