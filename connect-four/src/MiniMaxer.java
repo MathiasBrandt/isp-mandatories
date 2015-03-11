abf50c8ea6b8e4cbc6c2074a11fd65774f827867
Simon Langhoff <simlanghoff@gmail.com>
2015-03-11 18:23:35 +0100

    Added  stubs to implement for eval function.

--- connect-four/src/MiniMaxer.java
+++ connect-four/src/MiniMaxer.java
@@ -10,6 +10,7 @@ public class MiniMaxer {
     public final int PLAYER_MIN = 1;
     public final int PLAYER_MAX = 2;
     public int aiPlayerId;
+    public final int CUTOFF = 10;
 
     public MiniMaxer(int playerId) {
         aiPlayerId = playerId;
@@ -38,7 +39,7 @@ public class MiniMaxer {
                 // if the action is actually in the list of possible actions, i.e., if the column is NOT full
                 if(!state.isColumnFull(i)) {
                     // calculate the minimax value for this action
-                    minimaxValues[i] = maxValue(state.copyState(), alpha, beta);
+                    minimaxValues[i] = maxValue(state.copyState(), alpha, beta, 0);
                 } else {
                     // if the action is not in the list of possible actions, assign a value to indicate such.
                     minimaxValues[i] = Double.NaN;
@@ -63,7 +64,7 @@ public class MiniMaxer {
         else {
             for(int i = 0; i < state.getColumnCount(); i++) {
                 if(!state.isColumnFull(i)) {
-                    minimaxValues[i] = minValue(state.copyState(), alpha, beta);
+                    minimaxValues[i] = minValue(state.copyState(), alpha, beta, 0);
                 } else {
                     minimaxValues[i] = Double.NaN;
                 }
@@ -90,13 +91,11 @@ public class MiniMaxer {
      * @param beta the parent node's beta value
      * @return the lowest utility value
      */
-    private double minValue(GameState state, double alpha, double beta) {
-        double utility = getUtility(state);
-
-        // if the utility value is bigger than zero, it means that the game is over in the current state.
-        if(utility > 0) {
-            return utility;
+    private double minValue(GameState state, double alpha, double beta, int depth) {
+        if(cutoffTest(state, depth)){
+            return eval(state);
         }
+        depth++;
 
         // initialize with dummy value
         double value = Double.POSITIVE_INFINITY;
@@ -105,7 +104,7 @@ public class MiniMaxer {
         for (int i = 0; i < state.getColumnCount(); i++) {
             if (!state.isColumnFull(i)) {
                 state.insertCoin(i, PLAYER_MIN);
-                value = Double.min(value, maxValue(state.copyState(), alpha, beta));
+                value = Double.min(value, maxValue(state.copyState(), alpha, beta, depth));
 
                 // if the value is lower than the parent's alpha value, prune the tree
                 if(value <= alpha){
@@ -127,30 +126,78 @@ public class MiniMaxer {
      * @param beta the parent node's beta value
      * @return the highest utility value
      */
-    private double maxValue(GameState state, double alpha, double beta) {
+    private double maxValue(GameState state, double alpha, double beta, int depth) {
         // NOTE: comments have been omitted since they are more or less the same as in the minValue method
-        double utility = getUtility(state);
-        if(utility > 0) {
-            return utility;
+        if(cutoffTest(state, depth)){
+            return eval(state);
         }
+        depth++;
 
         double value = Double.NEGATIVE_INFINITY;
         for(int i = 0; i < state.getColumnCount(); i++) {
             if(!state.isColumnFull(i)) {
                 state.insertCoin(i, PLAYER_MAX);
-                value = Double.max(value, minValue(state.copyState(), alpha, beta));
-                System.out.println(value + " " + beta);
-
-                alpha = Double.max(alpha, value); // TODO: overall alpha or the one supplied?
+                value = Double.max(value, minValue(state.copyState(), alpha, beta, depth));
 
                 if(value >= beta){
                     return value;
                 }
 
+                alpha = Double.max(alpha, value);
             }
         }
 
         return value;
+    }
+
+    private boolean cutoffTest(GameState state, int depth) {
+        if(depth >= CUTOFF){
+            return true;
+        }
+
+        switch(state.gameFinished()) {
+            case PLAYER1:
+                return true;
+            case PLAYER2:
+                return true;
+            case TIE:
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Counts the maximum amount of coins that are connected in a row based upon the lastCoinPosition.
+     * @param state
+     * @return
+     */
+    public int maxCoinsInARow(GameState state){
+
+
+
+        return 0;
+    }
+
+    /**
+     *
+     * @param state
+     * @return amount of win combinations that the lastCoinPosition is a part of.
+     */
+    public int winCombinationsCount(GameState state){
+
+
+        return 0;
+    }
+
+    /**
+     * Looks up static evaluation table and returns a value based on the {@code} lastCoinPosition.
+     * @param state
+     * @return
+     */
+    public int coinPositionValue(GameState state){
+
+        return 0;
     }
 
     /**
@@ -159,7 +206,14 @@ public class MiniMaxer {
      * @param state the current state
      * @return the utility value of the winning player, or -1 if the game is not over
      */
-    private double getUtility(GameState state) {
+    private double eval(GameState state){
+        //
+        int score = 0;
+
+        score += maxCoinsInARow(state);
+        score += winCombinationsCount(state);
+        score += coinPositionValue(state);
+
         switch(state.gameFinished()) {
             case PLAYER1:
                 return UTILITY_MIN;
